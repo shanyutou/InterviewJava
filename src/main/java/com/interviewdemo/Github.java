fbc1316cf77249cb592a45e2efd6a5ee6fb1cb55
jmme <jmme@foxmail.com>
2021-08-11 22:42:31 +0800

    github end

---
+++ src/main/java/com/interviewdemo/Github.java
@@ -0,0 +1,34 @@
+package com.interviewdemo;
+
+/**
+ * in
+ *  XXX关键词 in:name、discription、readme
+ *  eg: seckill in:name,readme,discription
+ *
+ *  stars    eg: springboot stars:>=5000    springboot stars:1000..5000
+ *
+ *
+ *  fork      eg springboot forks:>=5000     springboot forks:100..500
+ *
+ *  Awesome
+ *          Awesome  + 关键词 比如redis
+ *
+ *
+ *  #L
+ *          地址 + #L13
+ *          地址 + #L13-L20
+ *
+ *  项目内搜索   英文字母 t
+ *             其它快捷键
+ *
+ *
+ *  location:地区
+ *  language:语言
+ *
+ *
+ *
+ *
+ */
+
+public class Github {
+}
