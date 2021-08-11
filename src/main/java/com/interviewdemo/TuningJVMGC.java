bed8d0336cb0ddab5f3bd5941831cef102c54fcd
jmme <jmme@foxmail.com>
2021-08-11 17:47:18 +0800

    bug jstack not run

--- src/main/java/com/interviewdemo/TuningJVMGC.java
+++ src/main/java/com/interviewdemo/TuningJVMGC.java
@@ -16,7 +16,18 @@ package com.interviewdemo;
  * 硬盘：df
  * 磁盘IO： iostat -xdk 2 3
  * 网络IO： ifstat
+ *
+ * jps
+ * ps -mp 进程 -o THREAD,tid,time
+ * jstack tid | grep tid(16) -A60 ******没有成功，计划换成oracle JDK试试******
  */
 
 public class TuningJVMGC {
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println(new java.util.Random().nextInt(77778888));
+        }
+    }
+
 }
